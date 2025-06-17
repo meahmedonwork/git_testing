5ea56204bbde68e6db7cd8126a5f3558d97e1f70
meahmedonwork <meahmedonwork@gmail.com>
2025-06-17 09:30:17 +0500

    modified gitignore

---
+++ lib/ahmed/ahmed.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ahmed extends StatelessWidget {
+  const Ahmed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

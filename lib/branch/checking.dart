3b31e65a66da5010fea457cd561ada884d0f5c78
meahmedonwork <meahmedonwork@gmail.com>
2025-06-17 12:00:03 +0500

    add styling to the container

--- lib/branch/checking.dart
+++ lib/branch/checking.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Checking extends StatelessWidget {
   const Checking({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: CupertinoColors.white,
+      child: Column(
+        children: [
+          
+          Text('HEllO', style: TextStyle(fontSize: 20, color: Colors.red),)
+        ],
+      ),
+      
+    );
   }
 }

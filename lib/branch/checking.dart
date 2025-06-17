b1587c17543dfcc96d5fe9bbec403600ae1f187f
meahmedonwork <meahmedonwork@gmail.com>
2025-06-17 11:30:10 +0500

    added branch dir to lib

---
+++ lib/branch/checking.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Checking extends StatelessWidget {
+  const Checking({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
